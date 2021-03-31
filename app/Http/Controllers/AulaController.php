3e508d7dfecdda7c9b92eb5bacd273b751ef640c
LEANDRO\Leandro L. Godoy <leandrolenon@gmail.com>
2021-03-30 21:46:24 -0300

    Segundo commit

--- app/Http/Controllers/AulaController.php
+++ app/Http/Controllers/AulaController.php
@@ -1,26 +1,72 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
 
 class AulaController extends Controller
 {
     public function show($id = null)
     {
-        echo 'exibe um ou todos os recursos';
+        if ($id) {
+            $aulas = \App\Models\Aula::find($id);
+        }else {
+            $aulas = \App\Models\Aula::all();
+        }
+        
+        return response($aulas, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+   
+    public function create(Request $request)
+    {
+        if (isset($request->nome)){
+            $aula = new \App\Models\Aula();
+            $aula->nome = $request->nome;
+            $aula->save();
+            return response($aula, 201, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'nome da aula nao foi informada'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 
-    public function create()
+    public function update(Request $request, $id)
     {
-        echo 'cria um recurso';
-    }
-
-    public function update($id)
-    {
-        echo 'altera um recurso existente';
+        if (isset($request->nome) && $id ){
+            $aula = \App\Models\Aula::find($id);
+            $aula->nome = $request->nome;
+            $aula->save();
+            return response($aula, 200, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'nome da aula nao foi informada ou id não informado'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 
     public function destroy($id)
     {
-        echo 'deleta um recurso existente ';
+        if (isset($id)) {
+            $aula = \App\Models\Aula::find($id);
+            if (is_null($aula))
+            throw new \Exception("A aula solicitada nao existe!");           
+            return response(true, 200, [
+                'Content-Type' => 'application/json'
+            ]);
+        }
+        return response([
+            'error' => 'id nao foi informado'
+        ], 404, [
+            'Content-Type' => 'application/json'
+        ]);
     }
 }
